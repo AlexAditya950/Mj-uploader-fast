f0c63226c5337a8cf0afa3ff0d7e00fd30e38996
꧁༒♛Bihari ♛༒꧂ <abhi736902@gmail.com>
2026-03-18 11:24:34 +0530

    Refactor API and configuration variables

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abhi736902:Abhishek
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7881009185"))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "7881009185").split()]  # Default to owner ID
 
 # Channel Configuration
